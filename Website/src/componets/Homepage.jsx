2aaa79d1eaa3e8adfd0a053a521f0404e8b39903
Bhavya Patel <bhvayapatel4907@gmail.com>
2026-01-23 15:47:20 +0530

    Homepages Css almost done

--- Website/src/componets/Homepage.jsx
+++ Website/src/componets/Homepage.jsx
@@ -1,15 +1,54 @@
 import NavBarChe from "./NavBarChe.jsx";
-import NavBar from "./NavBarChe.jsx";
+import FrontPageBasic from "./FrontPageBasic.jsx";
 
 
 function Homepage() {
+
+  const infoItems = [
+    {
+      title: "Spice levels",
+      value: "Mild • Medium • Hot",
+    },
+    {
+      title: "Preparation",
+      value: "Slow Cooked • Charcoal",
+    },
+    {
+      title: "Origin",
+      value: "Traditional Recipes",
+    },
+  ];
+
   return (
     <>
-        <NavBarChe />
+      <NavBarChe />
 
-        <div className="h-[1000px]" ></div>
-        <div 
-        className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-black via-black/60 to-transparent backdrop-blur-sm pointer-events-none z-20"
+      <div className="h-[1000px] relative bg-black/100" >
+        <div className="absolute inset-0 h-screen w-full bg-[url('/muttonCurryPhoto4.jpg')] bg-cover bg-center opacity-40 flex"></div>
+
+        <div className="relative h-screen w-full z-20 p-35 pl-50 flex flex-col  gap-5">
+          <div className="text-BrightWritingColor bg-white/8 mr-auto text-[12px] font-medium font- p-2 py-1 rounded-full border border-white/20">Rustic • Bold • Fiery <span className="text-orange-500">•</span> Non-veg curries</div>
+
+          <h1 className="text-BrightWritingColor text-6xl font-medium">Kook du Ku Curries <br /> where spice stays honest.</h1>
+
+          <div className="text-white/80 text-[19px]">Chicken, mutton, and seafood—slow-simmered, charcoal-kissed, and served with <br />a heat you can feel.</div>
+
+          <div className="flex gap-3 text-BrightWritingColor">
+            <button className="bg-[rgb(214,58,31)] hover:bg-orange-700/90 p-3 rounded-full px-5 text-[15px] font-medium cursor-pointer">Order Now</button>
+            <button className="bg-white/12 hover:bg-white/15 p-3 rounded-full px-5 text-[15px] font-medium border border-white/10 cursor-pointer">Explore Menu</button>
+          </div>
+
+          <div className="text-white flex gap-5">
+            {infoItems.map((item, index) => (
+              <FrontPageBasic key={index} title={item.title} value={item.value} />
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* <img src="/muttonCurryPhoto3.png" alt="" className="h-[100vh] w-full object-cover opacity-[52%]" /> */}
+
+      <div
+        className="absolute bottom-0 left-0 w-full h-55 bg-gradient-to-t from-black via-black/60 to-black/0  pointer-events-none z-20"
       ></div>
     </>
   )
--- Website/src/componets/NavBarChe.jsx
+++ Website/src/componets/NavBarChe.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 function NavBarChe() {
   return (
     <>
-        <nav className="bg-black/80 backdrop-blur-md text-lightWritingColor flex justify-around text-[15px] items-center p-4 fixed w-full top-0 z-10">
-            <div className="flex justify-center gap-5 items-center">
+        <nav className="bg-black/60 backdrop-blur-md text-lightWritingColor flex justify-around text-[15px] items-center p-4 fixed w-full top-0 z-30">
+            <div className="flex justify-center gap-5 items-center cursor-pointer">
                 <img src="#" alt="Logo" />
-                <div>
+                <div >
                     <div className="restoName text-BrightWritingColor font-medium">Kook Du Cu Curries</div>
                     <div className="mineDescrip text-[#818189] text-[12px]">Curries • Non-veg • Spicy</div>
                 </div>
             </div>  
 
             <div>
-                <ol className='flex justify-center gap-5'>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>Contant</li>
+                <ol className='flex justify-center gap-5 '>
+                    <li className='cursor-pointer hover:text-white/50'>Home</li>
+                    <li className='cursor-pointer hover:text-white/50'>Menu</li>
+                    <li className='cursor-pointer hover:text-white/50'>Contant</li>
                 </ol>
             </div>
 
             <div className='flex gap-5'>
-                <button className='bg-[#2a2b2d]/80 p-2 rounded-full px-3 text-BrightWritingColor font-medium'>Order Now</button>
-                <button className='bg-[#2a2b2d]/80 p-2 rounded-full px-3' ><img src="#" alt="#" /></button>
+                <button className='bg-[#2a2b2d]/80 hover:bg-white/12 p-2 rounded-full px-3 text-BrightWritingColor font-medium border border-white/10 cursor-pointer'>Order Now</button>
+                <button className='bg-[#2a2b2d]/80 p-2 rounded-full px-3 cursor-pointer' ><img src="#" alt="#" /></button>
             </div>
 
         </nav>
