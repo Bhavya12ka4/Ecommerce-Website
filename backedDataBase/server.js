76cd854af6f85d37e07a1bd15d8b2aae4673d622
Bhavya Patel <bhvayapatel4907@gmail.com>
2026-02-01 21:16:06 +0530

    add title to the page

--- backedDataBase/server.js
+++ backedDataBase/server.js
@@ -635,7 +635,12 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: '*', // Allows all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/images', express.static('images'));
 
